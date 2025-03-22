7ede31a1f748d4947d56fbd46b49143c5c959e19
Pratiphal2324 <pratiphalchapagain1000@gmail.com>
2025-03-22 10:53:55 +0545

    Started Hangman GUI

---
+++ hangman_gui.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+root = tk.Tk()
+root.geometry('800x500')
+root.title("Hangman Game")
+label = tk.Label(root, text = "Let's Play Hangman!", font = ('Monofur',30))
+label.pack()
+root.mainloop()
